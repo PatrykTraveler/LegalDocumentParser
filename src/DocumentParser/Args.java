6655006e45bab015bc5e77cd8164cd495374deb7
PatrykTraveler <31049319+PatrykTraveler@users.noreply.github.com>
2018-01-05 23:12:14 +0100

    Changed description of options

--- src/DocumentParser/Args.java
+++ src/DocumentParser/Args.java
@@ -10,14 +10,14 @@ public class Args {
     @Parameter
     private List<String> parameters = new ArrayList<>();
 
-    @Parameter(names = {"-f", "--file"}, description = "Load file by typing path to it", arity = 1, required = true)
+    @Parameter(names = {"-f", "--file"}, description = "Load file by typing path to it -f [Path to file]", arity = 1, required = true)
     private String path;
-    @Parameter(names = {"-e", "--element"}, description = "Element you want to print, example:\n for content mode: [\"Art. 1, 1., 1\"])" +
-            "\nfor table of content mode: [Rozdział X | DZIAŁ X]", arity = 1)
+    @Parameter(names = {"-e", "--element"}, description = "[Optional] Element you want to print, example:\n for content mode: -e \"Art. 1, 1., 1)\"" +
+            "\nfor table of content mode: -e \"Rozdział X\"", arity = 1)
     private String elements;
-    @Parameter(names = {"-r","--range"}, description = "[ONLY FOR CONTENT MODE] Choose range of articles to print, example: [\"Art. 5.-Art. 10.\"]", arity = 1)
+    @Parameter(names = {"-r","--range"}, description = "[Optional][ONLY FOR CONTENT MODE] Choose range of articles to print, example: -e \"Art. 5.-Art. 10.\"", arity = 1)
     private String range;
-    @Parameter(names = {"-c","--content"}, description = "Table of content mode by default, [-c false] to switch to content mode")
+    @Parameter(names = {"-c","--content"}, description = "[Optional]Table of content mode by default, -c false to switch to content mode")
     private Boolean content = false;
     @Parameter(names = {"-h", "--help"}, description = "Remember to use quotation marks for arguments that contain white spaces", help = false)
     private boolean help = false;
